e48a41d866caad55c15538073bc55e49cba29974
ikram akbar <ikramakbar71@gmail.com>
2023-11-08 19:31:02 +0600

    few changed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-const axios = require('axios');
+
+const getData = async () => {
+    const res = await axios.get("https://crud.teamrabbil.com/api/v1/ReadProduct");
+    if (res.status === 200) {
+        let items = res.data.data;
+        for (const item of items) {
+            console.log(item);
+          document.getElementById("item-list").innerHTML += `
+          
+          <tr>
+             <td>${item.ProductCode}</td>
+             <td>${item.ProductName}</td>
+             <td>${item.Qty}</td>
+             <td>${item.UnitPrice}</td>
+             <td>${item.TotalPrice}</td>
+             <td><button class="button button-outline">Update</button></td>
+             <td><button class="button button-outline">Delete</button></td>
+             
+          </tr>
+          `
+        }
+    }
+    else {
+        console.log("error")
+    }
+}
+getData();
